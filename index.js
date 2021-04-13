246fc682beeda98b019b7f61dd25187b2c738d75
EduGro <73360551+EduGro@users.noreply.github.com>
2021-04-12 22:26:22 -0500

    Updated index.js

--- index.js
+++ index.js
@@ -26,11 +26,14 @@ app.get('/', (req,res) => {
  * @swagger
  * /login:
  *  get:
- *      description: get the login page
+ *      description: get the login page (MongoDB)
  *      responses:
  *          200:
  *              description: html page with the log in
+ *          500:
+ *              description: server error
  */
+
 app.get('/login', (req, res) => {
     res.send('login');
 });
@@ -39,7 +42,7 @@ app.get('/login', (req, res) => {
  * @swagger
  * /login:
  *  post:
- *      description: send the login data to the server
+ *      description: send the login data to the server (MongoDB)
  *      parameters:
  *          - in : body
  *            name: credentials
@@ -49,8 +52,10 @@ app.get('/login', (req, res) => {
  *      responses:
  *          200:
  *              description: redirects to principal
- *          403:
+ *          401:
  *              description: access denied, sends the user an alert that credentials aren't valid
+ *          500:
+ *              description: server error
  */
 app.post('/login', (req, res) => {
     res.send('login');
@@ -60,10 +65,12 @@ app.post('/login', (req, res) => {
  * @swagger
  * /registro:
  *  get:
- *      description: get the registro page
+ *      description: get the registro page (MongoDB)
  *      responses:
  *          200:
  *              description: html page with the registro page 
+ *          500:
+ *              description: server error
  */
 app.get('/registro', (req, res) => {
     res.send('registro');
@@ -73,7 +80,7 @@ app.get('/registro', (req, res) => {
  * @swagger
  * /registro:
  *  post:
- *      description: sends the user's new credentials
+ *      description: sends the user's new credentials (MongoDB)
  *      parameters:
  *          - in : body
  *            name: credentials 
@@ -85,11 +92,111 @@ app.get('/registro', (req, res) => {
  *              description: redirects to principal
  *          409:
  *              description: Conflict, sends the user an alert that the user already exists
+ *          500:
+ *              description: server error
  */
 app.post('/registro', (req, res) => {
     res.send('registro');
+});
+
+/**
+ * @swagger
+ * /main:
+ *  get:
+ *      description: get the main page and displays the recipes (EDAMAM)
+ *      responses:
+ *          200:
+ *              description: gets EDAMAM and puts it in the html
+ *          500:
+ *              description: server error
+ */
+app.get('/main', (req, res) => {
+    res.send('main');
+});
+
+/**
+ * @swagger
+ * /recipe:
+ *  get:
+ *      description: gets the recipe by name (EDAMAM)
+ *      parameters:
+ *          - in : body
+ *            name: recipeName
+ *            description: get the whole recipe
+ *            schema:
+ *              type: string
+ *      responses:
+ *          200:
+ *              description: returns and puts the recipe
+ *          500:
+ *              description: server error
+ */
+app.get('/recipe', (req, res) => {
+    res.send('recipe');
+});
+
+/**
+ * @swagger
+ * /lists:
+ *  get:
+ *      description: gets the recipe by name (MongoDB)
+ *      parameters:
+ *          - in : body
+ *            name: userID
+ *            description: gets the users lists
+ *            schema:
+ *              type: number
+ *      responses:
+ *          200:
+ *              description: returns the users lists if any available
+ *          500:
+ *              description: server error
+ */
+app.get('/lists', (req, res) => {
+    res.send('list');
+});
+
+/**
+ * @swagger
+ * /lists:id:
+ *  get:
+ *      description: gets the recipes in that ID created (MongoDB) & (EDAMAM)
+ *      parameters:
+ *          - in : url
+ *            name: listId
+ *            description: The id of the list created by the user
+ *            schema:
+ *              type: number
+ *      responses:
+ *          200:
+ *              description: returns the id from the recipes that the user added and then returns all the recipes
+ *          500:
+ *              description: server error
+ */
+app.get('/lists:id', (req, res) => {
+    res.send('list-details');
 })
 
+/**
+ * @swagger
+ * /users:
+ *  get:
+ *      description: get the user details and the users list (MongoDB)
+ *      parameters:
+ *          - in : body
+ *            name: userID
+ *            description: get the users information
+ *            schema:
+ *              type: number
+ *      responses:
+ *          200:
+ *              description: reeturns the users information as well as its created playlists
+ *          500:
+ *              description: server error
+ */
+app.get('/users', (req, res) => {
+    res.send('users');
+})
 //Swagger
 const swaggerDoc = swaggerJsDoc(swaggerOptions);
 
@@ -107,10 +214,12 @@ app.listen(port, () => {
  *      parameters:
  *          - in : body|query|url
  *            name: param name
- *            description: param descriptikon
+ *            description: param description
  *            schema:
  *              type: string|number|boolean|object
  *      responses:
  *          200:
  *              description: success call to the endpoint
+ *          500:
+ *              description: server error
  */
